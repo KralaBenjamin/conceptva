9c0369d4b8135b39eff98027468989b53f60b1e1
jonitro165 <42643475+Jonitr0@users.noreply.github.com>
2021-11-16 15:57:02 +0100

    Polygon test

--- map_view.py
+++ map_view.py
@@ -1,12 +1,13 @@
 import io
 import sys
 
-import xarray
 import folium
 import sqlite3
 import pandas as pd
 from PySide2 import QtWidgets, QtWebEngineWidgets, QtCore
 from dataclasses import dataclass
+import geopandas as gpd
+from shapely.geometry import Polygon
 
 start_coords = [54.12, 8.37]
 min_time = QtCore.QDateTime(QtCore.QDate(2013, 1, 1), QtCore.QTime(0, 0))
@@ -16,10 +17,12 @@ max_time = QtCore.QDateTime(QtCore.QDate(2013, 12, 31), QtCore.QTime(23, 59))
 begin_start_time = QtCore.QDateTime(QtCore.QDate(2013, 6, 1), QtCore.QTime(0, 0))
 begin_end_time = QtCore.QDateTime(QtCore.QDate(2013, 6, 2), QtCore.QTime(0, 0))
 
+
 @dataclass
 class map_data:
-    data_obs: pd.DataFrame = None
-    data_bw: pd.DataFrame = None
+    data_obs: pd.DataFrame = pd.DataFrame()
+    data_bw: pd.DataFrame = pd.DataFrame()
+    data_fw: pd.DataFrame = pd.DataFrame()
 
 
 class map_view(QtWidgets.QMainWindow):
@@ -32,12 +35,23 @@ class map_view(QtWidgets.QMainWindow):
         self.web_view.loadFinished.connect(lambda: self.update_finished())
         self.start_datetime_edit = QtWidgets.QDateTimeEdit()
         self.end_datetime_edit = QtWidgets.QDateTimeEdit()
-        self.data_map = dict()
+
+        # load data
+        print("loading")
+        self.runtime_ds = map_data()
+        db = sqlite3.connect("data/data_test.db")
+        query_obs = "SELECT * FROM OBS"
+        self.runtime_ds.data_obs = pd.read_sql_query(query_obs, db)
+        query_bw = "SELECT * FROM BW"
+        self.runtime_ds.data_bw = pd.read_sql_query(query_bw, db)
+        query_fw = "SELECT * FROM FW"
+        self.runtime_ds.data_fw = pd.read_sql_query(query_fw, db)
+        print("loading complete")
 
         self.setCentralWidget(self.create_gui())
 
     # returns an object (currently "struct" of dataframes) which contains the data relevant for the given time
-    def get_dateframe_for_time_string(self, start_datetime: QtCore.QDateTime, end_datetime: QtCore.QDateTime):
+    def get_data_for_time_range(self, start_datetime: QtCore.QDateTime, end_datetime: QtCore.QDateTime):
         start_time_str = self.create_time_string(start_datetime)
         end_time_str = self.create_time_string(end_datetime)
 
@@ -48,9 +62,13 @@ class map_view(QtWidgets.QMainWindow):
         query_obs = "SELECT * FROM OBS WHERE CAST(time as INT) BETWEEN " + start_time_str + " AND " + end_time_str
         m_data.data_obs = pd.read_sql_query(query_obs, db)
 
-        query_bw = "SELECT * FROM BW WHERE CAST(label as INT) BETWEEN " + str(
-            int(m_data.data_obs['label'].min())) + " AND " + str(int(m_data.data_obs['label'].max()))
-        m_data.data_bw = pd.read_sql_query(query_bw, db)
+        # TODO: make this work properly and preferably faster
+        try:
+            query_bw = "SELECT * FROM BW WHERE CAST(label as INT) BETWEEN " + str(
+                int(m_data.data_obs['label'].min())) + " AND " + str(int(m_data.data_obs['label'].max()))
+            m_data.data_bw = pd.read_sql_query(query_bw, db)
+        except:
+            print("loading interpolated data failed")
 
         db.close()
         return m_data
@@ -82,24 +100,23 @@ class map_view(QtWidgets.QMainWindow):
         end_datetime = self.end_datetime_edit.dateTime()
 
         # get data
-        m_data = self.get_dateframe_for_time_string(start_datetime, end_datetime)
-        print("loaded")
+        m_data = self.get_data_for_time_range(start_datetime, end_datetime)
 
         # rebuild map
         self.fol_map = folium.Map(location=start_coords, zoom_start=10)
         # place markers on map
-        self.add_markers(m_data.data_obs, "#cf5a30")
-        self.add_markers(m_data.data_bw, "#55b33b")
-        print("painted")
+        #self.add_markers(m_data.data_obs, "#cf5a30")
+        #self.add_markers(m_data.data_bw, "#55b33b")
+
+        self.draw_polygon(m_data.data_bw, "#55b33b")
 
         # convert map to bytes and set html to webview
         data = io.BytesIO()
         self.fol_map.location = start_coords
         self.fol_map.save(data, close_file=False)
         html = data.getvalue().decode()
-        #self.write_html_to_file(html)
+        self.write_html_to_file(html)
         self.web_view.setHtml(html)
-        print("update done")
 
     # for debugging
     def write_html_to_file(self, html):
@@ -114,6 +131,14 @@ class map_view(QtWidgets.QMainWindow):
             folium.vector_layers.CircleMarker(
                 location=coords, radius=5, color=color, fill=True, fillOpacity=1.0, fillColor=color
             ).add_to(self.fol_map)
+
+    def draw_polygon(self, df, color):
+        lat_point_list = df['latitude'].tolist()
+        lon_point_list = df['longitude'].tolist()
+
+        polygon_geom = Polygon(zip(lon_point_list, lat_point_list))
+        polygon_geom = polygon_geom.convex_hull
+        folium.GeoJson(polygon_geom).add_to(self.fol_map)
 
     def update_finished(self):
         # update label
